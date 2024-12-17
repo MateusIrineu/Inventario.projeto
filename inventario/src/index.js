d22f231604b23457f8161088e4e1f5a3e1557e1e
Mateus Irineu <irineu.mateus11@gmail.com>
2024-12-17 17:15:42 -0300

    .

--- inventario/src/index.js
+++ inventario/src/index.js
@@ -1,1 +1,33 @@
-console.log("oie")
+import PromptSync from "prompt-sync";
+
+const prompt = PromptSync();
+
+const menuPrincipal = () => {
+    console.clear();
+  
+    console.log("Sistema de Gerenciamento de Inventário");
+   
+    console.log("1 - Gerenciar Produtos");
+    console.log("2 - Gerenciar Categorias");
+    console.log("3 - Gerar Relatórios");
+    console.log("0 - Sair");
+  
+
+    const opcao = prompt("Escolha a opção desejada: ");
+    switch(opcao) {
+        case "1":
+            // gerenciarProdutos();
+            // break;
+        case "2":
+            // gerenciarCategorias();
+            // break;
+        case "3":
+            // gerarRelatorios();
+            // break;
+        case "0":
+            process.exit();
+    }
+}
+
+export default menuPrincipal;
+menuPrincipal();
